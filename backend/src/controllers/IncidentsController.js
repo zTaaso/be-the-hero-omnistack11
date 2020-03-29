279cb8a2b092a7ce828431f81e0ccbb3f3da890b
zTaaso <ztaaso.cedaspy@gmail.com>
2020-03-29 13:45:04 -0300

    Aplication mobile

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -4,16 +4,16 @@ module.exports = {
   async index(req, res) {
     const { page = 1 } = req.query;
 
-    const [count] = await connection('incidents').count();
-
-
     const incidents = await connection('incidents')
       .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
-
-    res.header('X-Total-Count', count['count(*)'])
+    
+    const [count,] = await connection('incidents').count();
+    res.header('X-Total-Count', count['count(*)'] - 1)
+    //console.log({ counti: count, countasterico: count['count(*)'] })
+    
     return res.json(incidents)
   },
 
